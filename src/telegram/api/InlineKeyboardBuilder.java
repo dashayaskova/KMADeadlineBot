dc32dc81b4d73d0ff77385b457df5f83f9c5c54e
mike-kob <37714867+mike-kob@users.noreply.github.com>
2018-07-07 18:29:57 +0300

    Buttons in messages

---
+++ src/telegram/api/InlineKeyboardBuilder.java
@@ -0,0 +1,56 @@
+package telegram.api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+public class InlineKeyboardBuilder {
+
+    private Long chatId;
+    private String text;
+
+    private List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+    private List<InlineKeyboardButton> row = new ArrayList<>();
+
+    private InlineKeyboardBuilder() {}
+
+    public static InlineKeyboardBuilder create(Long chatId) {
+        InlineKeyboardBuilder builder = new InlineKeyboardBuilder();
+        builder.chatId = chatId;
+        return builder;
+    }
+
+    public InlineKeyboardBuilder setText(String text) {
+        this.text = text;
+        return this;
+    }
+
+    public InlineKeyboardBuilder nextRow() {
+    	keyboard.add(row);
+        this.row = new ArrayList<>();
+        return this;
+    }
+
+    public InlineKeyboardBuilder addButton(String text, String callbackData) {
+        row.add(new InlineKeyboardButton().setText(text).setCallbackData(callbackData));
+        return this;
+    }
+
+    public SendMessage build() {
+        SendMessage message = new SendMessage();
+
+        message.setChatId(chatId);
+        message.setText(text);
+
+        InlineKeyboardMarkup keyboardMarkup = new InlineKeyboardMarkup();
+
+        keyboardMarkup.setKeyboard(keyboard);
+        message.setReplyMarkup(keyboardMarkup);
+
+        return message;
+    }
+
+}
